7e928958a109ec4221bd85d7cc4d3b6a511c17d4
Koller, Ute <u.koller@oth-aw.de>
2022-05-29 13:49:12 +0200

    Added canvas
    Former-commit-id: 8b62cf6e96d7110bdb9a6d1bd52457054af53427

--- sys-src/frontend/src/App.js
+++ sys-src/frontend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Canvas from './components/Canvas.jsx'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Racing Lines</h1>
+        <Canvas/>
       </header>
+      
     </div>
   );
 }
---
+++ sys-src/frontend/src/components/Canvas.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Stage } from '@inlet/react-pixi'
+
+function Canvas(){
+    return(
+        <>
+            <Stage>
+                
+            </Stage>
+        </>
+    );
+}
+
+export default Canvas;
