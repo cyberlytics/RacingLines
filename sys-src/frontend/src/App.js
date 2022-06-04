77128e58e4e9bf486855dc440b5b5a330fdd7ea8
Kern, Samuel <s.kern@oth-aw.de>
2022-06-04 16:45:19 +0200

    Added Socket.io lobby system, routing, material ui (Pair Programming mit Christoffer Mayer)
    Former-commit-id: 38d0a21bca19ef94f86661bb27d72dcb29a24713

--- sys-src/frontend/src/App.js
+++ sys-src/frontend/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Lobby from "./Lobby";
+import Home from "./Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Racing Lines</h1>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Routes>
+                <Route exact path="/Home" element={<Home/>}/>
+                <Route path="/Lobby" element={<Lobby/>}/>
+                <Route path="*" element={<Home/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
