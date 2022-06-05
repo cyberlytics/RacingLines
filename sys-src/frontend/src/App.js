f90e3ba8ef572daa0efe34f57edb75eb8787614e
Kern, Samuel <s.kern@oth-aw.de>
2022-06-05 23:29:44 +0200

    Created the game component. The Game can be played by one person now. TODO: Check collision for player trails. Player leaves not one trail but more trail segments
    Former-commit-id: 9c77f3276e91d01beec6f6f3f5aa2f45648d1afa

--- sys-src/frontend/src/App.js
+++ sys-src/frontend/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Lobby from "./Lobby";
 import Home from "./Home";
+import Game from "./components/Game";
 
 const App = () => {
     return (
         <div>
             <Routes>
                 <Route exact path="/Home" element={<Home/>}/>
-                <Route path="/Lobby" element={<Lobby/>}/>
+                <Route path="/Lobby" element={<Game/>}/>
                 <Route path="*" element={<Home/>}/>
             </Routes>
         </div>
