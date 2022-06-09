fae619c5eb223cccf0e336e2a25f2d871b5deb05
Fabig, Anika <a.eder1@oth-aw.de>
2022-06-09 15:26:03 +0200

    Inserted the lobby components (that are implemented at this point) into a grid system for a nicer layout

--- sys-src/frontend/src/Lobby.js
+++ sys-src/frontend/src/Lobby.js
@@ -5,6 +5,8 @@ import {makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import CopyLink from "./components/CopyLink";
 import DifficultyLevel from "./components/DifficultyLevel";
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -20,6 +22,20 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'center',
     }
 }));
+
+
+//create our styles
+const grid_classes = {
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: 20,
+      textAlign: "center",
+      color: "blue",
+      fontFamily: "Roboto"
+    }
+  };
 
 const socket = io.connect("http://localhost:3001");
 
@@ -63,16 +79,48 @@ const Lobby = () => {
 
     return (
         <div>
-            <div className={classes.paper}>
-                <h1>Counter</h1>
-                <h1>{number}</h1></div>
-            <div className={classes.button}>
-                <Button variant="contained" className={classes.button} color="primary" type="button"
-                        onClick={handleAddButtonClick}>add</Button>
-                <Button variant="contained" color="secondary" type="button" onClick={handleResetButtonClick}>reset</Button>
+            
+            <div style={grid_classes.root}>
+            <Grid container spacing={3}>
+                {/*For example,This item will be 12 units wide on extra small screens */}
+                <Grid item xs={12}>
+                    <Paper style={grid_classes.paper}>
+                        <CopyLink/>
+                    </Paper>
+                </Grid>
+                {/*This item will be 12 units on extra small screens */}
+                {/*But will be 6 units on small screens */}
+                <Grid item xs={12} sm={6}>
+                    <Paper style={grid_classes.paper}>
+                        <div className={classes.paper}>
+                            <h1>Counter</h1>
+                            <h1>{number}</h1></div>
+                        <div className={classes.button}>
+                            <Button variant="contained" className={classes.button} color="primary" type="button"
+                                    onClick={handleAddButtonClick}>add</Button>
+                            <Button variant="contained" color="secondary" type="button" onClick={handleResetButtonClick}>reset</Button>
+                        </div>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Paper style={grid_classes.paper}>
+                        <DifficultyLevel/>
+                    </Paper>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                <Paper style={grid_classes.paper}>xs=6 sm=3</Paper>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                <Paper style={grid_classes.paper}>xs=6 sm=3</Paper>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                <Paper style={grid_classes.paper}>xs=6 sm=3</Paper>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                <Paper style={grid_classes.paper}>xs=6 sm=3</Paper>
+                </Grid>
+            </Grid>
             </div>
-            <CopyLink/>
-            <DifficultyLevel/>
         </div>
     );
 }
