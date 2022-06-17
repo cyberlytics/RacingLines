89567e4c0ce4491dee66c94bf564a8e7a61c502a
Koller, Ute <u.koller@oth-aw.de>
2022-06-17 18:09:05 +0200

    Added styling to scoreboard
    Former-commit-id: a1c16a2e64a8270320e93b26586699404ea6e4c6

--- sys-src/frontend/src/components/Scoreboard.jsx
+++ sys-src/frontend/src/components/Scoreboard.jsx
@@ -1,7 +1,38 @@
 import { useEffect, useState } from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import { Box, Typography, Container } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    scoreboard:{
+        width: '20%',
+        border: '2px solid black',
+        borderRadius: 10,        
+        background: "#f50057", fontWeight: 'bold'
+    },
+    scoreheader:{
+        margin: 10, 
+        fontSize: 25,    
+        textAlign: 'center',
+        textDecoration: 'underline'       
+    },
+    playerscore:{
+        fontSize: 16,
+        margin: 10,
+        background: 'FFFFFF'        
+    },
+    name:{
+        display: 'inline',
+    },
+    score:{
+        display: 'inline',
+        marginLeft: '70%'
+    }
+});
 
 export default function Scoreboard({ gameSetUp }) {
     const [players, setPlayers] = useState(gameSetUp.players);
+
+    const classes = useStyles();
 
     useEffect(() => {
         // Subscribe each player for changes
@@ -30,12 +61,15 @@ export default function Scoreboard({ gameSetUp }) {
 
     return (
         <>
-            <h2>Scoreboard</h2>
-            {players.map((player) => (
-                <div key={player.id}>
-                    <p>{`${player.name}: ${player.score}`}</p>
+        <div className={classes.scoreboard}>        
+            <h2 className={classes.scoreheader}>SCOREBOARD</h2>
+           {players.map((player) => (
+                <div className={classes.playerscore} key={player.id}>
+                    <p className={classes.name}>{`${player.name}: `}</p>
+                    <p className={classes.score}>{`${player.score}`}</p>
                 </div>
             ))}
+        </div>
         </>
     );
 }
