8deffcc166b5557bc12ebd6bc57e2be6d17c23b8
Kern, Samuel <s.kern@oth-aw.de>
2022-06-17 14:50:49 +0200

    Improved the server communication. The player position will now be emitted to all other players in the room instead of the inputs.

--- sys-src/frontend/src/components/Game.js
+++ sys-src/frontend/src/components/Game.js
@@ -1,113 +1,139 @@
-import {useCallback, useEffect} from "react";
-import {GameManager} from "../util/GameManager";
+import { useCallback, useEffect } from "react";
+import { GameManager } from "../util/GameManager";
 import io from "socket.io-client";
-import {useSearchParams} from "react-router-dom";
-import {Player} from "../util/Player";
+import { useSearchParams } from "react-router-dom";
+import { Player } from "../util/Player";
 
-const socket = io.connect(process.env.REACT_APP_IPAddress+":3001");
+const socket = io.connect(process.env.REACT_APP_IPAddress + ":3001");
 
 const Game = () => {
+  // default boardSize
+  const boardSize = 800;
 
-    // boardSize before prop is passed in
-    const boardSize = 800;
+  const GamMan = new GameManager(socket, boardSize);
 
-    const GamMan = new GameManager(socket, boardSize);
+  //get room from url and join room
+  const [roomParam] = useSearchParams();
+  const room = roomParam.get("room");
+  GamMan.ServerCommunicationHelper.joinRoom(room);
 
-    //get room from url and join room
-    const [roomParam] = useSearchParams();
-    const room = roomParam.get("room");
-    GamMan.ServerCommunicationHelper.joinRoom(room);
-
-
-    useEffect(() => {
-        socket.on('playerInput', (data) => {
-            let LeftKeyPressed = data.inputLeft;
-            let RightKeyPressed = data.inputRight;
-            GamMan.inputDictionary[data.playerId] = {LeftKeyPressed, RightKeyPressed};
-        });
-
-        socket.on('gameStarted', (data) => {
-            let clientDictionary = data.clientDictionary;
-
-            Object.entries(clientDictionary).forEach(([key, value]) => {
-                GamMan.players.push(new Player("Player" , key, "pink", "blue",value.x, value.y, value.direction));
-                GamMan.inputDictionary[key] = {LeftKeyPressed: false, RightKeyPressed: false};
-            });
-        });
-
-        socket.on("connect", () => {
-            GamMan.clientId = socket.id;
-        });
-    }, [socket]);
-
-    //get input for left and right from the keyboard for the player direction
-    useEffect(() => {
-        document.addEventListener("keyup", (event) => {
-            //input variables for the client
-            let RightKeyPressed = false;
-            let LeftKeyPressed = false;
-            if (event.key === "ArrowLeft" || event.key === "a" || event.key === "A") {
-                LeftKeyPressed = false;
-            } else if (event.key === "ArrowRight" || event.key === "d" || event.key === "D") {
-                RightKeyPressed = false;
-            }
-            GamMan.inputDictionary[GamMan.clientId]={LeftKeyPressed, RightKeyPressed};
-            GamMan.ServerCommunicationHelper.sendInput(GamMan.inputDictionary[GamMan.clientId].LeftKeyPressed, GamMan.inputDictionary[GamMan.clientId].RightKeyPressed, GamMan.clientId);
-        });
+  useEffect(() => {
+    socket.on("newPlayerState", (data) => {
+      for (let i = 0; i < GamMan.players.length; i++) {
+        if (GamMan.players[i].id === data.playerId) {
+          console.log("updating player");
+          GamMan.players[i].addToPlayerStateBuffer(
+            data.positionX,
+            data.positionY,
+            data.isDrawing
+          );
+        }
+      }
     });
 
-    useEffect(() => {
-        document.addEventListener("keydown", (event) => {
-            if (event.key === "w") {
-                GamMan.ServerCommunicationHelper.startGame()
-            }
-        });
+    socket.on("gameStarted", (data) => {
+      let clientDictionary = data.clientDictionary;
+      Object.entries(clientDictionary).forEach(([key, value]) => {
+        let player = new Player(
+          "player",
+          key,
+          "green",
+          "blue",
+          value.x,
+          value.y,
+          value.direction
+        );
+        player.addToPlayerStateBuffer(value.x, value.y, true);
+        GamMan.players.push(player);
+        GamMan.gameRunning = true;
+      });
     });
 
-    //get input for left and right from the keyboard for the player direction
-    useEffect(() => {
-        document.addEventListener("keydown", (event) => {
-            //input variables for the client
-            let RightKeyPressed = false;
-            let LeftKeyPressed = false;
-            if (event.key === "ArrowLeft" || event.key === "a" || event.key === "A") {
-                LeftKeyPressed = true;
-            } else if (event.key === "ArrowRight" || event.key === "d" || event.key === "D") {
-                RightKeyPressed = true;
-            }
-            //set input for the player in the input dictionary with the player id as a key
-            GamMan.inputDictionary[GamMan.clientId]={LeftKeyPressed, RightKeyPressed};
-            GamMan.ServerCommunicationHelper.sendInput(GamMan.inputDictionary[GamMan.clientId].LeftKeyPressed, GamMan.inputDictionary[GamMan.clientId].RightKeyPressed, GamMan.clientId);
-        });
+    socket.on("connect", () => {
+      GamMan.clientId = socket.id;
     });
+  }, [socket]);
 
-    //Game loop
-    //Draw the game and move the player with a tick rate of 60 times per second
-    useEffect(() => {
-        const tick = setInterval(() => {
-            //get canvas and context
-            //const canvas = document.querySelector('canvas');
-            const canvasPl = document.getElementById("cvPlayers");
-            const ctxPl = canvasPl.getContext('2d');
+  let RightKeyPressed = false;
+  let LeftKeyPressed = false;
 
-            const canvasLi = document.getElementById("cvLines");
-            const ctxLi = canvasLi.getContext('2d');
-
-            let varNow =  new Date().getTime();
-            let deltaTime = (varNow - GamMan.lastTick) / 1000;
-            GamMan.lastTick = varNow;
-            GamMan.drawLines(canvasLi, ctxLi);
-            GamMan.drawPlayers(canvasPl, ctxPl);
-            GamMan.updateGameState(deltaTime, ctxLi);
-        }, 1000 / 60);
-        return () => clearInterval(tick);
+  //get input for left and right from the keyboard for the player direction
+  useEffect(() => {
+    document.addEventListener("keyup", (event) => {
+      if (event.key === "ArrowLeft" || event.key === "a" || event.key === "A") {
+        LeftKeyPressed = false;
+      } else if (
+        event.key === "ArrowRight" ||
+        event.key === "d" ||
+        event.key === "D"
+      ) {
+        RightKeyPressed = false;
+      }
+      GamMan.setClientInput(LeftKeyPressed, RightKeyPressed);
     });
+  });
 
-    return (
-        <header>
-            <canvas id={"cvPlayers"} width={boardSize} height={boardSize} style={{zIndex:2, position: 'absolute', top: 0, left: 0}}></canvas>
-            <canvas id={"cvLines"} width={boardSize} height={boardSize} style={{zIndex:1, position: 'absolute', top: 0, left: 0}}></canvas>
-        </header>
-    )
-}
+  //get input for left and right from the keyboard for the player direction
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "ArrowLeft" || event.key === "a" || event.key === "A") {
+        LeftKeyPressed = true;
+      } else if (
+        event.key === "ArrowRight" ||
+        event.key === "d" ||
+        event.key === "D"
+      ) {
+        RightKeyPressed = true;
+      }
+      //set input for the player in the input dictionary with the player id as a key
+      GamMan.setClientInput(LeftKeyPressed, RightKeyPressed);
+    });
+  });
+
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "w") {
+        GamMan.setUpRound();
+      }
+    });
+  });
+
+  //Draw the game and move the player with a tick rate of 60 times per second
+  useEffect(() => {
+    const tick = setInterval(() => {
+      //get canvas and context
+      //const canvas = document.querySelector('canvas');
+      const canvasPl = document.getElementById("cvPlayers");
+      const ctxPl = canvasPl.getContext("2d");
+
+      const canvasLi = document.getElementById("cvLines");
+      const ctxLi = canvasLi.getContext("2d");
+
+      let varNow = new Date().getTime();
+      let deltaTime = (varNow - GamMan.lastTick) / 1000;
+      GamMan.lastTick = varNow;
+      GamMan.drawLines(canvasLi, ctxLi);
+      GamMan.drawPlayers(canvasPl, ctxPl);
+      GamMan.updateGameState(deltaTime, ctxLi);
+    }, 1000 / 60);
+    return () => clearInterval(tick);
+  });
+
+  return (
+    <header>
+      <canvas
+        id={"cvPlayers"}
+        width={boardSize}
+        height={boardSize}
+        style={{ zIndex: 2, position: "absolute", top: 0, left: 0 }}
+      ></canvas>
+      <canvas
+        id={"cvLines"}
+        width={boardSize}
+        height={boardSize}
+        style={{ zIndex: 1, position: "absolute", top: 0, left: 0 }}
+      ></canvas>
+    </header>
+  );
+};
 export default Game;
--- sys-src/frontend/src/util/Player.js
+++ sys-src/frontend/src/util/Player.js
@@ -1,36 +1,58 @@
 export class Player {
-    constructor(name, id, circleColor, wallColor, positionX, positionY, directionAngle) {
-        this.name = name;
-        this.id = id;
-        this.size = 5;
-        this.directionAngle = directionAngle;
-        this.circleColor = circleColor;
-        this.lineColor = wallColor;
-        this.score = 0;
-        this.speed = 100;
-        this.isAlive = true;
-        this.positionY = positionY;
-        this.positionX = positionX;
-        this.lastY = positionY;
-        this.lastX = positionX;
-        this.isDrawing = true;
-    }
+  constructor(
+    name,
+    id,
+    circleColor,
+    wallColor,
+    positionX,
+    positionY,
+    directionAngle
+  ) {
+    this.name = name;
+    this.id = id;
+    this.size = 5;
+    this.directionAngle = directionAngle;
+    this.circleColor = circleColor;
+    this.lineColor = wallColor;
+    this.score = 0;
+    this.speed = 100;
+    this.isAlive = true;
+    this.positionY = positionY;
+    this.positionX = positionX;
+    this.lastY = positionY;
+    this.lastX = positionX;
+    this.isDrawing = true;
+    this.playerStateBuffer = [];
+  }
 
-    //move the player in the direction he is facing with the player speed with the delta time of the game
-    move(deltaTime) {
-        if(this.isAlive) {
-            this.positionX += this.speed * Math.cos(this.directionAngle ) * deltaTime;
-            this.positionY += this.speed * Math.sin(this.directionAngle) * deltaTime;
-        }
+  //move the player in the direction he is facing with the player speed with the delta time of the game
+  move(deltaTime) {
+    if (this.isAlive) {
+      this.positionX += this.speed * Math.cos(this.directionAngle) * deltaTime;
+      this.positionY += this.speed * Math.sin(this.directionAngle) * deltaTime;
     }
+  }
 
-    updateDirection(rightKeyPressed, leftKeyPressed) {
-        if (leftKeyPressed === true) {
-            this.directionAngle -= Math.PI / 35;
-        }
-        if (rightKeyPressed === true) {
-            this.directionAngle += Math.PI / 35;
-        }
-
+  updateDirection(inputLeft, inputRight) {
+    if (inputLeft === true) {
+      this.directionAngle -= Math.PI / 35;
     }
+    if (inputRight === true) {
+      this.directionAngle += Math.PI / 35;
+    }
+  }
+
+  addToPlayerStateBuffer(X, Y, drawing) {
+    this.playerStateBuffer.push({
+      positionX: X,
+      positionY: Y,
+      isDrawing: drawing,
+    });
+  }
+
+  setPlayerState(X, Y, isDrawing) {
+    this.positionX = X;
+    this.positionY = Y;
+    this.isDrawing = isDrawing;
+  }
 }
