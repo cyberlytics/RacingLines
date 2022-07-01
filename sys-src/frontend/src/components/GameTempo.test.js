16fc1b7f14d98cc69fc0d374d5dcbf2bca40f8e3
Koller, Ute <u.koller@oth-aw.de>
2022-07-01 11:48:22 +0200

    Added testes in GameTempo.test.js Changed values for radiobuttons in GameTempo.js

--- sys-src/frontend/src/components/GameTempo.test.js
+++ sys-src/frontend/src/components/GameTempo.test.js
@@ -20,3 +20,37 @@ describe("Test if default value is checked and the others not", () => {
     })
 })
 
+describe("Test if checked radiobutton changes and method gets called", () => {
+    test("Test if methos handleGameTempoChanged gets called", () => {
+        const parentMethod = jest.fn();
+
+        const {getByLabelText} = render(<GameTempo parentMethod={parentMethod}/>);
+        fireEvent.click(getByLabelText('Fast'), {target: {value: 'fast'}});
+        expect(parentMethod).toHaveBeenCalled();
+
+    })
+    test("Test if checked radiobutton changes when click radiobutton for fast", () => {
+        const parentMethod = jest.fn();
+
+        const {getByLabelText} = render(<GameTempo parentMethod={parentMethod}/>);
+        fireEvent.click(getByLabelText('Fast'), {target: {value: 'fast'}});
+
+        expect(getByLabelText('Fast')).toBeChecked();
+    })
+    test("Test if checked radiobutton changes when click radiobutton for slow", () => {
+        const parentMethod = jest.fn();
+
+        const {getByLabelText} = render(<GameTempo parentMethod={parentMethod}/>);
+        fireEvent.click(getByLabelText('Slow'), {target: {value: 'slow'}});
+        
+        expect(getByLabelText('Slow')).toBeChecked();
+    })
+    test("Test if checked radiobutton changes when click radiobutton for normal", () => {
+        const parentMethod = jest.fn();
+
+        const {getByLabelText} = render(<GameTempo parentMethod={parentMethod}/>);
+        fireEvent.click(getByLabelText('Normal'), {target: {value: 'normal'}});
+        
+        expect(getByLabelText('Normal')).toBeChecked();
+    })
+})
