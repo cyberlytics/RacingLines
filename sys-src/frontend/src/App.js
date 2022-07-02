6e829c12f258a955b964b969e622135be395fab5
Gast, Alina <a.gast@oth-aw.de>
2022-07-02 17:23:02 +0200

    Fixing Merge Conflicts in App.js

--- sys-src/frontend/src/App.js
+++ sys-src/frontend/src/App.js
@@ -1,21 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-import Canvas from './components/Canvas';
-import KeyBindings from './components/KeyBindings';
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Racing Lines</h1>
-        <Canvas/>
-         </header>
-     <KeyBindings/>
-    </div>
-  );
-=======
 import {Route, Routes} from "react-router-dom";
 import Lobby from "./Lobby";
 import Home from "./Home";
@@ -37,7 +20,6 @@ const App = () => {
             </Routes> 
         </div>
     );
->>>>>>> 9c8f0c6f527a485bf8ee42c7dacb826c951f0245
 }
 
 
