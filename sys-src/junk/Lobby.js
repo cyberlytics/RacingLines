dae204d6122b1efd92b00eddb647d06480d4c000
Fabig, Anika <a.eder1@oth-aw.de>
2022-07-04 08:53:51 +0200

    Added the code for "Update game size and game tempo for all players when a player changes the selection" to the junk folder

---
+++ sys-src/junk/Lobby.js
@@ -0,0 +1,225 @@
+import React, {useEffect, useState, useRef, Component} from 'react';
+import {useNavigate, useSearchParams} from "react-router-dom";
+import io from 'socket.io-client';
+import {makeStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import CopyLink from "./components/CopyLink";
+import CanvasSize from "./components/CanvasSize";
+import GameTempo from "./components/GameTempo";
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import PlayerColors from './components/PlayerColors';
+import { TextField } from '@material-ui/core';
+import {v4 as uuidv4} from 'uuid';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(10),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    button: {
+        marginBottom: theme.spacing(2),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }
+}));
+
+
+//create our styles
+const grid_classes = {
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: 20,
+      textAlign: "left",
+      color: "blue",
+      fontFamily: "Roboto"
+    },
+    player:{
+      padding: 20,
+      marginBottom: 10,
+      color: "blue",
+      textAlign: "left",
+      fontFamily: "Roboto"
+    }
+  };
+    const socket = io.connect(process.env.REACT_APP_IPAddress+":3001");
+
+
+    const Lobby = () => {
+
+    const classes = useStyles();
+
+    const [playerList,setPlayerList] = useState([]);
+    const [settingsList, setSettingsList] = useState([]);
+
+    const [roomParam] = useSearchParams();
+    const room = roomParam.get("room");
+    const clientNameRef = useRef();
+
+    useEffect(() => {
+
+        const joinRoom = () => {
+            if (room !== "") {
+                socket.emit("join_lobby", {room});
+                console.log("joined room");
+            }
+        };
+        joinRoom();
+    }, [roomParam]);
+
+    useEffect(() => {
+        socket.on('onPlayerJoin', data => {
+            //console.log(data.joinedPlayers);
+            setPlayerList(data.joinedPlayers);
+        });
+        socket.on('onPlayerLeave', (data) => {
+            setPlayerList(data.joinedPlayers);
+        });
+        socket.on('onPlayerNameChanged', (data) => {
+            setPlayerList(data.joinedPlayers);
+            console.log(data.joinedPlayers.Name)
+        });
+        socket.on('onPlayerColorChanged', (data) => {
+            setPlayerList(data.joinedPlayers);
+        });
+        socket.on('onCanvasSizeChanged', (data) => {
+            setSettingsList(data.roomGameSettings);
+        });
+        socket.on('onGameTempoChanged', (data) => {
+            setSettingsList(data.roomGameSettings);
+        });
+    }, [socket]);
+
+    useEffect(() => {
+        return  Object.keys(playerList).map((item,index) => {
+            return(
+                <option value={playerList[item].Id} key={index}>
+                    {playerList[item].Name}
+                </option>
+            )
+        })
+        //add a TextField for the player in the jsx below
+
+
+
+   }, [playerList]);
+
+   useEffect(() => {
+    return  Object.keys(settingsList).map((item,index) => {
+        return(
+            <option>
+                <option value={settingsList[item].CanvasSize} key={index}>
+                    {settingsList[item].CanvasSize}
+                </option>
+                <option value={settingsList[item].GameTempo} key={index}>
+                {settingsList[item].GameTempo}
+                </option>
+            </option>
+        )
+    })
+
+}, [settingsList]);
+
+        function handleClientNameChanged(event){
+            const newPlayerList = {...playerList};
+            console.log(newPlayerList[socket.id].Name);
+            newPlayerList[socket.id].Name = event.target.value;
+            setPlayerList(newPlayerList);
+            console.log(newPlayerList[socket.id].Name);
+            socket.emit("playerNameChanged", {playerList, room});
+        }
+
+        function handleClientColorChanged(event){
+            const newPlayerList = {...playerList};
+            console.log(newPlayerList[socket.id].PlayerColor);
+            newPlayerList[socket.id].PlayerColor = event.target.value;
+            setPlayerList(newPlayerList);
+            console.log(newPlayerList[socket.id].PlayerColor);
+            socket.emit("playerColorChanged", {playerList, room});
+        }
+
+        function handleCanvasSizeChanged(event){
+            const newSettingsList = {...settingsList};
+            console.log(newSettingsList[room.id].CanvasSize);
+            newSettingsList[room.id].CanvasSize = event.target.value;
+            setSettingsList(newSettingsList);
+            console.log(newSettingsList[room.id].CanvasSize);
+            socket.emit("canvasSizeChanged", {settingsList, room});
+        }
+
+        function handleGameTempoChanged(event){
+            const newSettingsList = {...settingsList};
+            console.log(newSettingsList[room.id].GameTempo);
+            newSettingsList[room.id].GameTempo = event.target.value;
+            setSettingsList(newSettingsList);
+            console.log(newSettingsList[room.id].GameTempo);
+            socket.emit("gameTempoChanged", {settingsList, room});
+        }
+
+        const navigate = useNavigate();
+        function startGame(event){
+            //WIP
+        }
+
+        return (
+            <div>
+                <div style={grid_classes.root}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <Paper style={grid_classes.paper}>
+                                <CopyLink/>
+                            </Paper>
+                        </Grid>
+                        {/*This item will be 12 units on extra small screens */}
+                        {/*But will be 6 units on small screens */}
+                        <Grid id="playerNames" item xs={12} sm={6}>
+
+                                <ul>{Object.keys(playerList).map((item,index) => {
+                                    if(item===socket.id)return(
+                                        <li key={item} style={{color: playerList[item].PlayerColor, fontSize: 35}}>
+                                            <Paper style={grid_classes.player}>
+                                            <TextField id={item} value={playerList[item].Name} variant="outlined" onChange={handleClientNameChanged}/>
+                                            </Paper>
+                                        </li>
+                                    )
+                                    else return(
+                                        <li key={item} style={{color: playerList[item].PlayerColor, fontSize: 35}}>
+                                            <Paper style={grid_classes.player}>
+                                            <TextField id={item} value={playerList[item].Name} variant="outlined" disabled={true}/>
+                                        </Paper>
+                                        </li>
+                                    )})}
+                                </ul>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Grid container spacing={3}>
+                                <Grid item xs={12}>
+                                    <Paper style={grid_classes.paper}>
+                                        <CanvasSize parentMethod={handleCanvasSizeChanged}/>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Paper style={grid_classes.paper}>
+                                        <GameTempo parentMethod={handleGameTempoChanged}/>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Paper style={grid_classes.paper}>
+                                        <PlayerColors parentMethod={handleClientColorChanged}/>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button onClick={startGame} variant="contained" className={classes.button} color="primary" type="button">Play</Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+        );
+}; export default Lobby;
---
+++ sys-src/junk/index.js
@@ -0,0 +1,223 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Score = require("./models/score");
+const Poisson = require("poisson-disk-sampling");
+require("dotenv").config();
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.IPAddress + ":3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+server.listen(3001, () => {
+  console.log("Server is running on " + process.env.IPAddress + ":3001");
+});
+
+//connect to mongodb
+const dbURI =
+  "mongodb+srv://rluser:RacingLines123@cluster0.vehm5.mongodb.net/RacingLinesDatabase?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("This is the Express Server!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/add-score", (req, res) => {
+  const score = new Score({
+    roomID: "new scoreID",
+    playerID: "new playerID",
+    score: 20,
+  });
+
+  score
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-scores", (req, res) => {
+  Score.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-score", (req, res) => {
+  Score.findById("62925edd58c810164db79158")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+let players = {};
+let gameSettings = {};
+
+io.on("connection", (socket) => {
+  socket.on("join_lobby", (data) => {
+    socket.join(data.room);
+    players[socket.id] = {
+      Id: socket.id,
+      Name: "player",
+      PlayerColor: "green",
+      LineColor: "black",
+    };
+
+    gameSettings[data.room] = {
+      CanvasSize: "medium",
+      GameTempo: "normal"
+    };
+
+    const clients = io.sockets.adapter.rooms.get(data.room);
+    const joinedPlayers = {};
+    const roomGameSettings = {};
+
+    console.log(clients);
+    if (clients !== undefined) {
+      clients.forEach((client) => {
+        joinedPlayers[client] = players[client];
+      });
+    }
+    if (clients !== undefined) {
+      clients.forEach((client) => {
+        roomGameSettings[client] = gameSettings[client];
+      });
+    }
+    socket.emit("onPlayerJoin", { joinedPlayers });
+    socket.in(data.room).emit("onPlayerJoin", { joinedPlayers });
+  });
+
+  socket.on("playerNameChanged", (data) => {
+    console.log(data.playerList.Name);
+    players[socket.id].Name = data.playerList[socket.id].Name;
+    const clients = io.sockets.adapter.rooms.get(data.room);
+    const joinedPlayers = {};
+    if (clients !== undefined) {
+      clients.forEach((client) => {
+        joinedPlayers[client] = players[client];
+      });
+    }
+    socket.in(data.room).emit("onPlayerNameChanged", { joinedPlayers });
+  });
+
+  socket.on("playerColorChanged", (data) => {
+    console.log(data.playerList.PlayerColor);
+    players[socket.id].PlayerColor = data.playerList[socket.id].PlayerColor;
+    const clients = io.sockets.adapter.rooms.get(data.room);
+    const joinedPlayers = {};
+    if (clients !== undefined) {
+      clients.forEach((client) => {
+        joinedPlayers[client] = players[client];
+      });
+    }
+    socket.in(data.room).emit("onPlayerColorChanged", { joinedPlayers });
+  });
+
+  socket.on("gameTempoChanged", (data) => {
+    console.log(data.playerList.GameTempo);
+    gameSettings[room.id].GameTempo = data.settingsList[room.id].GameTempo;
+    const clients = io.sockets.adapter.rooms.get(data.room);
+    const roomGameSettings = {};
+    if (clients !== undefined) {
+      clients.forEach((client) => {
+        roomGameSettings[client] = gameSettings[client];
+      });
+    }
+    socket.in(data.room).emit("onGameTempoChanged", { roomGameSettings });
+  });
+
+  socket.on("canvasSizeChanged", (data) => {
+    console.log(data.playerList.CanvasSize);
+    players[room.id].CanvasSize = data.settingsList[room.id].CanvasSize;
+    const clients = io.sockets.adapter.rooms.get(data.room);
+    const joinedPlayers = {};
+    if (clients !== undefined) {
+      clients.forEach((client) => {
+        joinedPlayers[client] = players[client];
+      });
+    }
+    socket.in(data.room).emit("onCanvasSizeChanged", { joinedPlayers });
+  });
+
+  socket.on("join_room", (data) => {
+    console.log("room joined");
+    socket.join(data);
+  });
+
+  socket.on("startGame", (data) => {
+    const clients = io.sockets.adapter.rooms.get(data.room);
+    const clientDictionary = {};
+    let minDist = 300;
+    let p = new Poisson({
+      shape: [600, 600],
+      minDistance: minDist,
+      tries: 10,
+    });
+    while (p.fill().length < clients.size) {
+      p = new Poisson({
+        shape: [600, 600],
+        minDistance: minDist,
+        tries: 10,
+      });
+      minDist -= 10;
+    }
+    console.log(clients.size + " " + p.fill().length);
+    console.log(p.fill());
+    let counter = 0;
+    clients.forEach((client) => {
+      let xposition = p.fill()[counter][0] + 100;
+      let yposition = p.fill()[counter][1] + 100;
+      console.log(
+        "x: " +
+          (p.fill()[counter][0] + 100) +
+          "y: " +
+          (p.fill()[counter][1] + 100)
+      );
+      clientDictionary[client] = {
+        x: xposition,
+        y: yposition,
+        direction: Math.floor(Math.random() * (360 - 1 + 1)) + 1,
+      };
+      counter += 1;
+    });
+    io.to(data.room).emit("gameStarted", { clientDictionary });
+  });
+
+  socket.on("playerState", (data) => {
+    let positionX = data.positionX;
+    let positionY = data.positionY;
+    let isDrawing = data.isDrawing;
+    let playerId = socket.id;
+    io.to(data.room).emit("newPlayerState", {
+      positionX,
+      positionY,
+      isDrawing,
+      playerId,
+    });
+  });
+});
